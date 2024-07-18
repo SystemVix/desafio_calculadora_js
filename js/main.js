17ba95e4476544862df47251c6896714604fee36
Herhos <herhos@outlook.com>
2024-07-18 15:25:28 -0300

    Captura e validação de números

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+// Captura do number1
+const number1 = document.getElementById("number1");
+number1.addEventListener('change', handleInputNumber);
+
+// Captura do number2
+const number2 = document.getElementById("number2");
+number2.addEventListener('change', handleInputNumber);
+
+// Validação se é numero (Prof. Nélio / StackOverFlow)
+function isNumber(numero)
+{
+   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// Validação de number1 e number2
+function handleInputNumber(event)
+{
+   if (isNumber(event.target.value))
+   {
+      event.target.classList.remove("input-error");
+   }
+   else
+   {
+      event.target.classList.add("input-error");
+   }
+}
+
+
 
